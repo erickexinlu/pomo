e3d4443efd360751835b80817d038d1fd356341e
erickexinlu <erickexinlu@gmail.com>
2022-09-25 01:39:27 -0700

    Implement JSON saving and functional to-do list

---
+++ todo.js
@@ -0,0 +1,80 @@
+window.onload=function(){
+    const form = document.querySelector('.js-form');
+    form.addEventListener('submit', eventHandler);
+}
+
+let todoItems = [];
+
+function renderTodo(todo) {
+    //JSON
+    localStorage.setItem('todoItemsRef', JSON.stringify(todoItems));
+
+    const list = document.querySelector('.js-todo-list');
+    const item = document.querySelector(`[data-key='${todo.id}']`);
+    const isChecked = todo.checked ? 'done': '';
+    const node = document.createElement("li");
+    node.setAttribute('class', `todo-item ${isChecked}`);
+    node.setAttribute('data-key', todo.id);
+    node.innerHTML = `
+        <input id="${todo.id}" type="checkbox"/>
+        <label for="${todo.id}" class="tick js-tick"></label>
+        <span>${todo.text}</span>
+    `;
+
+    if (item) {
+        list.replaceChild(node, item);
+    } else {
+        list.append(node);
+    }
+}
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  renderTodo(todo);
+}
+
+function toggleDone(key) {
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    todoItems[index].checked = !todoItems[index].checked;
+    renderTodo(todoItems[index]);
+}
+
+
+function eventHandler() {
+  // prevent page refresh on form submission
+  event.preventDefault();
+
+  const input = document.querySelector('.js-todo-input');
+
+
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
+}
+
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+    if (event.target.classList.contains('js-tick')) {
+        const itemKey = event.target.parentElement.dataset.key;
+        toggleDone(itemKey);
+    }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const ref = localStorage.getItem('todoItemsRef');
+    if (ref) {
+      todoItems = JSON.parse(ref);
+      todoItems.forEach(t => {
+        renderTodo(t);
+      });
+    }
+});
